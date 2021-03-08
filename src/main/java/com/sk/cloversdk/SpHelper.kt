f5598bd45fcdfcbde81efdb0fcf0df988cfbe815
shaokun <605416017@qq.com>
2021-03-08 15:41:35 +0800

    更换ipfs的地址

--- src/main/java/com/sk/cloversdk/SpHelper.kt
+++ src/main/java/com/sk/cloversdk/SpHelper.kt
@@ -5,7 +5,7 @@ import android.content.SharedPreferences
 
 object SpHelper : BaseHelper() {
     const val KEY_IPFS_URL = "key_IPFS_URL"
-    const val KEY_IPFS_URL_DEFAULT = "https://ipfs.clover.finance"
+    const val KEY_IPFS_URL_DEFAULT = "https://ipfs.glpro.io:4010"
 
     private fun getSp(): SharedPreferences {
         return ctx.getSharedPreferences(Constant.SP_NAME, Context.MODE_PRIVATE)
